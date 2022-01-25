b808ef03dfcaf0be96f157765ab1503d78b69973
Tim Kopp <timkopp@gmail.com>
2022-01-25 14:47:45 -0500

    unit tests refactor 7686

--- CyberScope/Scripts/Dashboard_Agency_Landing.js
+++ CyberScope/Scripts/Dashboard_Agency_Landing.js
@@ -1,11 +1,9 @@
-﻿$(document).ready(() => { 
-    for (var i = 2022; i > 2012; i--) {
-        $("#fy_filter").append(`<option value=${i}>${i}</option>`); 
+﻿$(document).ready(() => {
+    for (var i = 2022; i > 2010; i--) {
+        $("#fy_filter").append(`<option value=${i}>${i}</option>`)
     }
-    $("#fy_filter").val('2021');
-    $("#fy_filter").change(() => {
-        Render();
-    });
+    $("#fy_filter").val('2021').change(() => Render() );
+
     let request = {
         SPROC: "DashAgency",
         PARMS: {
@@ -15,27 +13,23 @@
     RequestDataTable( request, (response) => {
         $("#payload").val(response.d);
         Render();
-    }); 
+    });
+ 
 });
 const Render = () => {
-    const response_data = $("#payload").val();
-    const response_obj = JSON.parse(response_data);
+    const response_json = $("#payload").val();
+    const response_data = JSON.parse(response_json);
     const fy_filter_val = $("#fy_filter").val();
-    const fy_data = response_obj.reduce((result, item) => {
-        if (!result[item.Year]) {
-            result[item.Year] = [];
-        }
-        result[item.Year].push(item);
-        return result;
-    }, {});
-  
+ 
     let quart_data = { 1: { OT: 0, OD: 0 }, 2: { OT: 0, OD: 0 }, 3: { OT: 0, OD: 0 }, 4: { OT: 0, OD: 0 } };
-    const ycoords = fy_data[fy_filter_val].reduce((result, item) => {
-        result[item.ScheduledActivationQuarter].OT += item.ONTIME;
-        result[item.ScheduledActivationQuarter].OD += item.OVERDUE;
+    const ycoords = response_data.reduce((result, item) => {
+        if (fy_filter_val == item.Year) { 
+            result[item.ScheduledActivationQuarter].OT += item.ONTIME;
+            result[item.ScheduledActivationQuarter].OD += item.OVERDUE; 
+        }
         return result;
     }, quart_data);
-
+  
     Object.keys(quart_data).forEach(quart => {
         let trace1 = {
             type: 'bar',
