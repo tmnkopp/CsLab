be4c45a37e1c255d82d102a58c4d823bacb62412
Tim Kopp <timkopp@gmail.com>
2022-01-31 15:08:33 -0500

    unit tests refactor 11482

--- CyberScope/Scripts/dashboard/Agency_Landing_RolesPerms.js
+++ CyberScope/Scripts/dashboard/Agency_Landing_RolesPerms.js
@@ -1,10 +1,74 @@
  
 $(document).ready(async () => { 
    
-    // const data = await RequestDataTable({  SPROC: "DashAgency",  PARMS: {   MODE: "UP_DEAD"   }  }); 
-    // RenderRolesPerms(data);  
+
+    const access_code_data = await RequestDataTable({
+        SPROC: "DashAgency",
+        PARMS: {
+            MODE: "ACCESS_CODES"
+        }
+    });
+
+    const data = {
+        upd: access_code_data
+    }
+
+    RenderAccessCodes(data);
+
+    const agency_lead_poc_data = await RequestDataTable({
+        SPROC: "DashAgency",
+        PARMS: {
+            MODE: "AGENCY_LEAD_POC"
+        }
+    });
+
+    const pocdata = {
+        upd: agency_lead_poc_data
+    }
+
+    RenderAgencyLeadPOCs(pocdata);
+
 }); 
  
-const RenderRolesPerms = (data) => {   
-    // $("#up-roles-perms").kendoGrid({   dataSource: {   data: data   }   });    
-}   
+const RenderAccessCodes = (data) => {   
+    data = data.upd;
+    $(".access-wrapper .k-grid").html('<div id="access-code-grid"></div>');
+    $("#access-code-grid").kendoGrid({
+        columns: [{
+            field: "AccessCode"
+            , title: "Access Code"
+        }, {
+            field: "ApprovedDate"
+            , title: "Approved Date"
+            }
+        ],
+        dataSource: {
+            data: data
+        }
+    });
+}
+
+
+const RenderAgencyLeadPOCs = (data) => {
+    data = data.upd;
+    $(".poc-wrapper .k-grid").html('<div id="agency-lead-poc-grid"></div>');
+    $("#agency-lead-poc-grid").kendoGrid({
+        columns: [
+            {
+            field: "Name"
+            , title: "POC Name"
+            },
+            {
+            field: "Email"
+            , title: "Email"
+            },
+            {
+                field: "WorkPhone"
+                , title: "Phone"
+            }
+        ],
+        dataSource: {
+            data: data
+        }
+    });
+}
